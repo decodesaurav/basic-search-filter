cb02c4b755784183bb67bab717202533fb808876
decodesaurav <decodesaurav@gmail.com>
2022-11-15 16:16:02 +0545

    initial setup

---
+++ src/App.js
@@ -0,0 +1,30 @@
+import './App.css';
+import { Component } from 'react';
+import { CardList } from './components/card-list/card-list.component';
+
+class App extends Component {
+constructor(){
+  super();
+  this.state = {
+    monsters: [],
+    searchField: ''
+  }
+}
+componentDidMount(){
+  fetch('https://jsonplaceholder.typicode.com/users')
+  .then(response => response.json())
+  .then(users => this.setState({monsters: users}))
+}
+  render(){
+  return (
+    <div className="App">
+      <input type='search' placeholder='Search Monsters' onChange={
+        e => {this.setState({searchField: e.target.value});
+        }} />
+      <CardList monsters = {this.state.monsters}> </CardList>
+    </div>
+    );
+  }
+}
+
+export default App;
